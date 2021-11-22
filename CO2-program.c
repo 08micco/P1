d2847a2dda2ef674abc59bd3ba8baa588e68897c
MadsSR <madssr2@gmail.com>
2021-11-22 12:38:11 +0100

    add_plug is done

--- CO2-program.c
+++ CO2-program.c
@@ -5,6 +5,7 @@
 #include <time.h>
 
 #define APPLIANCE_MAX 6
+#define PLUGS_MAX 10
 
 enum appliances
 {
@@ -20,7 +21,7 @@ char *appliances_string[APPLIANCE_MAX] = {"empty", "Microwave", "Kettle", "Oven"
 
 struct appliance
 {
-    int appliances;
+    appliances id;
     double power_consumption;
 };
 typedef struct appliance appliance;
@@ -31,34 +32,22 @@ struct user_profile
 };
 typedef struct user_profile user_profile;
 
-appliance add_plug(appliance);
+void add_plug(appliance *);
 
 int main(void)
 {
-    srand(time(NULL));
-    user_profile user_profile;
-    appliance plug1;
-
-    plug1 = add_plug(plug1);
-
-    printf("\nPower usage of %s is %.2lf kWh.\n", appliances_string[plug1.appliances], plug1.power_consumption);
+    appliance plug[PLUGS_MAX];
 
     return EXIT_SUCCESS;
 }
 
-appliance add_plug(appliance plug)
+void add_plug(appliance *plug)
 {
-    int plug_type;
-    printf("\n%d | %s\n%d | %s\n%d | %s\n%d | %s\n%d | %s\n\nSelect operation: ",
+    printf("Add appliances from the list:\n%d | %s\n%d | %s\n%d | %s\n%d | %s\n%d | %s\n\nSelect appliance: ",
            microwave, appliances_string[microwave],
            kettle, appliances_string[kettle],
            oven, appliances_string[oven],
            refrigerator, appliances_string[refrigerator],
            freezer, appliances_string[freezer]);
-    scanf(" %d", &plug_type);
-
-    plug.appliances = plug_type;
-    plug.power_consumption = (rand() % 1700) + (float)rand() / RAND_MAX;
-
-    return plug;
+    scanf(" %d", &plug.appliance);
 }
