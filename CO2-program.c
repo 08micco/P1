4ec2ba57f5192c9708019913cf984a1eeb16f1cf
Charlotte Elleby <charlotteelleby@h291.aau1x.klient2.satellit.site.aau.dk>
2021-11-23 10:49:22 +0100

    User profile done

--- CO2-program.c
+++ CO2-program.c
@@ -28,16 +28,17 @@ typedef struct appliance appliance;
 
 struct user_profile
 {
-    unsigned int household_size;
+    int household_size;
     appliance plug[PLUGS_MAX];
 };
 typedef struct user_profile user_profile;
 
 /* prototypes */
-user_profile initialize_user_profile(user_profile);
-void add_plug(appliance *);
+user_profile initialize_user_profile(user_profile user, int *);
+void add_plug(user_profile, int);
 void compareFunction(user_profile, user_profile, appliance *);
 void printTips(appliance *);
+void print_break(void);
 
 /* Main program */
 int main(void)
@@ -45,75 +46,63 @@ int main(void)
     appliance higher_consumption[APPLIANCE_MAX];
     user_profile user;
     user_profile general;
+    int amount_of_plugs;
 
-    initialize_user_profile(user);
+    user = initialize_user_profile(user, &amount_of_plugs);
 
-    compareFunction(user, general, higher_consumption);
-    printTips(higher_consumption);
+    //compareFunction(user, general, higher_consumption);
+    //printTips(higher_consumption);
 
     return EXIT_SUCCESS;
 }
 
+/* Assigns appliance to plug */
 void add_plug(user_profile user, int plug_index)
 {
-    printf("Add appliances from the list:\n%d | %s\n%d | %s\n%d | %s\n%d | %s\n%d | %s\n\nSelect appliance: ",
+    printf("Add appliances from the list:\n%d | %s\n%d | %s\n%d | %s\n%d | %s\n%d | %s\nSelect appliance: ",
            microwave, appliances_string[microwave],
            kettle, appliances_string[kettle],
            oven, appliances_string[oven],
            refrigerator, appliances_string[refrigerator],
            freezer, appliances_string[freezer]);
 
-    scanf(" %d", user.plug[plug_index].id);
+    scanf(" %d", &user.plug[plug_index].id); /* Assigns appliance id to the related plug.*/
 }
 
-/* This function compares whether the users power consumption of all appliances are bigger og smaller than the average.
-The instances where the user power consumption is higher than average is sorted into an array of structs called higher_consumption. Opposite for lowerConsumption. */
-void compareFunction(user_profile user, user_profile general, appliance *higher_consumption)
+/* User profile informations is set here */
+user_profile initialize_user_profile(user_profile user, int *plug_index)
 {
-    appliance lowerConsumption[APPLIANCE_MAX];
-    int countHigh = 0, countLow = 0;
-
-    int i;
-    for (i = 1; i < APPLIANCE_MAX; i++)
-    {
-        /* Creates new array higher_consumption which contains power consumptions bigger than average */
-        if (user.appliances[i].power_consumption > general.appliances[i].power_consumption)
-        {
-            printf("");
-            higher_consumption[countHigh].power_consumption = user.appliances[i].power_consumption;
-            higher_consumption[countHigh].id = user.appliances[i].id;
-            countHigh++;
-        }
-        else
-        {
-            printf("");
-            lowerConsumption[countLow].power_consumption = user.appliances[i].power_consumption;
-            lowerConsumption[countLow].id = user.appliances[i].id;
-            countLow++;
-        }
-
-        /* Prints percentage of average */
-        printf("Consumption of your %s is %.2lf%% of the average.\n", appliances_string[i],
-               user.appliances[i].power_consumption / general.appliances[i].power_consumption * 100);
-    }
-}
-
-void printTips(appliance *higher_consumption)
-{
-    printf("Din mor er et tip");
-}
-
-user_profile initialize_user_profile(user_profile user) /* User profile informations is set here */
-{
-    printf("What is the size of the household?\n"); /* Scan for household size to user profile*/
+    printf("What is the size of the household?\n"); /* Scan for household size to the user*/
     printf("Number of peoples: ");
     scanf("%d", &user.household_size);
+    print_break();
 
-    while (run && plug_index < PLUGS_MAX) /* All the kitchen appliances is added to user profile here */
+    int run = 1;
+    int scan_input;
+    *plug_index = 1;
+
+    while (run && *plug_index < PLUGS_MAX) /* All the kitchen appliances is added to user profile here */
     {
-       /* add_plug(user, plug_index);*/
-       
-    }
+        add_plug(user, *plug_index);
+        print_break();
 
+        printf("Add one more appliance?:\n1 | Yes\n2 | No\nSelect option: ");
+        scanf("%d", &scan_input);
+        print_break();
+        if (scan_input == 1)
+        {
+            *plug_index += 1;
+            run = 1;
+        }
+        else
+            run = 0;
+    }
+    printf("\nplugs: %d  size %d.\n", *plug_index, user.household_size); /* Skal slettes senere */
     return user;
+}
+
+/* Print a breakline */
+void print_break(void)
+{
+    printf("----------------------------------------------------\n");
 }
