c0561971492db5daa5b040464a51fb1d41cfbc83
MadsSR <madssr2@gmail.com>
2021-12-06 10:02:40 +0100

    json fix

--- json-parse.c
+++ json-parse.c
@@ -1,19 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "json-parser-master/json.h"
-#include <sys/stat.h>
-
-/* COMPILE (Static link)
-
-    "   gcc -o json_parse -I.. json-parse.c json-parser-master/json.c -lm  "
-
-
-
-RUN:
-
-  " json_parse.exe data/user_data.json  "
-
- */
+#include "./tiny-json/tiny-json.h"
 
 #define APPLIANCE_MAX 6
 #define PLUGS_MAX 10
@@ -29,7 +16,7 @@ enum appliances
 typedef enum appliances appliances;
 
 char *appliances_string[APPLIANCE_MAX] = {"empty", "Microwave", "Kettle", "Oven", "Refrigerator", "Coffee machine"};
-
+//
 struct appliance
 {
     int id;
@@ -52,169 +39,55 @@ struct average_profile
 typedef struct average_profile average_profile;
 
 user_profile parse_json_data(user_profile);
-static void process_value(json_value *value, int depth);
 void print_user_struct(user_profile user);
 user_profile parse_json_user_data(user_profile user, json_value *value);
 
-static void print_depth_shift(int depth)
+typedef struct date
 {
-    int j;
-    for (j = 0; j < depth; j++)
-    {
-        printf(" ");
-    }
-}
-
-static void process_value(json_value *value, int depth);
-
-static void process_object(json_value *value, int depth)
-{
-    int length, x;
-    if (value == NULL)
-    {
-        return;
-    }
-    length = value->u.object.length;
-    for (x = 0; x < length; x++)
-    {
-        print_depth_shift(depth);
-        printf("object[%d].name = %s\n", x, value->u.object.values[x].name);
-        process_value(value->u.object.values[x].value, depth + 1);
-    }
-}
-
-static void process_array(json_value *value, int depth)
-{
-    int length, x;
-    if (value == NULL)
-    {
-        return;
-    }
-    length = value->u.array.length;
-    printf("array\n");
-    for (x = 0; x < length; x++)
-    {
-        process_value(value->u.array.values[x], depth);
-    }
-}
-
-static void process_value(json_value *value, int depth)
-{
-    if (value == NULL)
-    {
-        return;
-    }
-    if (value->type != json_object)
-    {
-        print_depth_shift(depth);
-    }
-    switch (value->type)
-    {
-    case json_none:
-        printf("none\n");
-        break;
-    case json_null:
-        printf("null\n");
-        break;
-    case json_object:
-        process_object(value, depth + 1);
-        break;
-    case json_array:
-        process_array(value, depth + 1);
-        break;
-    case json_integer:
-        printf("int: %10ld\n", (long)value->u.integer);
-        break;
-    case json_double:
-        printf("double: %f\n", value->u.dbl);
-        break;
-    case json_string:
-        printf("string: %s\n", value->u.string.ptr);
-        break;
-    case json_boolean:
-        printf("bool: %d\n", value->u.boolean);
-        break;
-    }
-}
+    int day;
+    int month;
+    int year;
+} date;
 
 int main(void)
 {
-    char *filename;
-    FILE *fp;
-    struct stat filestatus;
-    int file_size;
-    char *file_contents;
-    json_char *json;
-    json_value *value;
-    user_profile user;
 
-    file_size = filestatus.st_size;
-    file_contents = (char *)malloc(filestatus.st_size);
-    if (file_contents == NULL)
+    char *str = 0;
+    long length;
+
+    FILE *fp = fopen("user_data.json", "r");
+
+    if (fp)
     {
-        fprintf(stderr, "Memory error: unable to allocate %d bytes\n", file_size);
-        return 1;
+        fseek(fp, 0, SEEK_END);
+        length = ftell(fp);
+        fseek(fp, 0, SEEK_SET);
+        str = malloc(length);
+        if (str)
+            fread(str, 1, length, fp);
     }
-
-    fp = fopen("data/user_data.json", "rb");
-    if (fp == NULL)
-    {
-        fprintf(stderr, "Unable to open data file\n");
-        fclose(fp);
-        free(file_contents);
-        return 1;
-    }
-
-    fread(file_contents, file_size, 1, fp);
-
     fclose(fp);
 
-    json = (json_char *)file_contents;
-
-    value = json_parse(json, file_size);
-
-    if (value == NULL)
+    json_t mem[320];
+    json_t const *json = json_create(str, mem, sizeof mem / sizeof *mem);
+    if (!json)
     {
-        fprintf(stderr, "Unable to parse data\n");
-        free(file_contents);
-        exit(1);
+        puts("Error json create.");
+        return EXIT_FAILURE;
     }
 
-    process_value(value, 0);
+    json_t const *date = json_getProperty(json, "01-12-2021");
+    json_t const *dateChild;
+    for (dateChild = json_getChild(date); dateChild != 0; dateChild = json_getSibling(dateChild))
+    {
+        json_t const *appliance_id = json_getProperty(dateChild, "appliance_id");
+        int const appliance_id_value = (int)json_getInteger(appliance_id);
 
-    json_value_free(value);
-    free(file_contents);
+        json_t const *power_consumption = json_getProperty(dateChild, "power_consumption");
+        double const power_consumption_value = (double)json_getReal(power_consumption);
 
-    user = parse_json_user_data(user, value);
-    print_user_struct(user);
+        printf("ID: %d | Power Consumption: %f\n", appliance_id_value, power_consumption_value);
+    }
 
     return EXIT_SUCCESS;
-}
-
-user_profile parse_json_user_data(user_profile user, json_value *value)
-{
-
-    user.household_size = 120;
-
-    /*    for (i; i < PLUGS_MAX; i++)
-    {
-        user.plug[i].id = ;
-        user.plug[i].power_consumption = ;
-    } */
-    return user;
-}
-
-json_parse_ex()
-
-    void print_user_struct(user_profile user)
-{
-
-    int i = 0;
-
-    printf("Household size: %d\n", user.household_size);
-
-    for (i; i < PLUGS_MAX; i++)
-    {
-        printf("Index: %d | Plug ID: %s | Power Consumption: %f kWh\n", i, appliances_string[user.plug[i].id], user.plug[i].power_consumption);
-    }
 }
--- sim_el.c
+++ sim_el.c
@@ -4,9 +4,8 @@
 #include <string.h>
 #include <time.h>
 #include <sys/stat.h>
-#include "json-parser-master/json.c"
 
-#define APPLIANCE_MAX 6
+#define APPLIANCE_MAX 5
 #define PLUGS_MAX 10
 
 enum appliances
@@ -19,12 +18,12 @@ enum appliances
 };
 typedef enum appliances appliances;
 
-char *appliances_string[APPLIANCE_MAX] = {"empty", "Microwave", "Kettle",
-                                          "Oven", "Refrigerator", "Freezer"};
+char *appliances_string[APPLIANCE_MAX + 1] = {"empty", "microwave", "kettle",
+                                              "oven", "refrigerator", "freezer"};
 
 struct appliance
 {
-    appliances id;
+    int id;
     double power_consumption;
 };
 typedef struct appliance appliance;
@@ -41,16 +40,19 @@ struct average_data
 };
 typedef struct average_data average_data;
 
-void write_appliance_data_to_file(FILE *, user_data);
-
+void print_data(user_data);
+user_data initialize_user_data(user_data, double[]);
 double random_num(double, double);
+void write_appliance_data_to_file(FILE *, user_data);
+int calc_time(struct tm *, int, int);
 
 int main()
 {
-
     FILE *data_file;
-
     user_data user;
+    srand(time(NULL)); /* Skal den være her??? */
+    /* Har skrevet det hele om så vi ikke får brug for dem her.
+    Lader dem lige være her in case vi skal se/bruge dem på et tidspunkt
 
     double min_microwave = 0.1115;
     double max_microwave = 0.4460;
@@ -61,40 +63,114 @@ int main()
     double min_refrigerator = 0.5023;
     double max_refrigerator = 0.5552;
     double min_coffeemachine = 0.1578;
-    double max_coffeemachine = 0.6311;
-    int count = 1;
+    double max_coffeemachine = 0.6311; */
 
-    srand(time(NULL));
+    /* Min and max values for each appliance */
+    double min_max_appliances[APPLIANCE_MAX * 2] = {
+        /* Microwave */
+        0.1115,
+        0.4460,
+        /* Kettle */
+        0.1890,
+        0.7560,
+        /* Oven */
+        0.4438,
+        1.4793,
+        /* Refigerator */
+        0.5023,
+        0.5552,
+        /* Freezer */
+        0.1578,
+        0.6311};
 
-    for (int i = 0; i < count; i++)
-    {
-        printf(" Microwave consumption: %f kWh\n Kettle consumption: %f kWh\n Oven consumption: %f kWh\n Refrigerator consumption: %f kWh\n Coffee machine consumption: %f kWh", random_num(max_microwave, min_microwave), random_num(max_kettle, min_kettle), random_num(max_oven, min_oven), random_num(max_refrigerator, min_refrigerator), random_num(max_coffeemachine, min_coffeemachine));
-    }
+    user = initialize_user_data(user, min_max_appliances);
+    print_data(user);
+
+    write_appliance_data_to_file(data_file, user);
 
     return 0;
 }
 
+/* Prints user_data */
+void print_data(user_data data)
+{
+    int i;
+    for (i = 0; i < APPLIANCE_MAX; i++)
+    {
+        printf("%s: id = %d, consumption = %f kWh\n", appliances_string[i + 1], data.plug[i].id, data.plug[i].power_consumption);
+    }
+}
+
+/* Initializes user_data by adding all appliances with an ID and power_consumption */
+user_data initialize_user_data(user_data data, double min_max_appliances[])
+{
+    int i;
+    for (i = 1; i <= APPLIANCE_MAX; i++)
+    {
+
+        data.plug[i - 1].id = i;
+        data.plug[i - 1].power_consumption = random_num(min_max_appliances[i * 2 - 1], min_max_appliances[i * 2]);
+    }
+
+    return data;
+}
+
+/* Generates a random number between two doubles */
 double random_num(double max, double min)
 {
+    /* Den returnerer ik et tal mellem min og max ved køleskabet??? */
     return (rand() / (RAND_MAX / (max - min))) + min;
 }
 
 /* This function writes household size, appliance number and their power consumption to a file */
+/* Lots of spaces and newlines printed to file to make it more readable */
 void write_appliance_data_to_file(FILE *file, user_data user)
 {
-    int i;
-    file = fopen("data/test.txt", "w+");
+    int i, x, days = 8; /* Days er midlertidig */
+    /* Creates or edits new file user_data.json under the data folder */
 
+    time_t now = time(NULL);
+    struct tm *current_time = localtime(&now);
+
+    file = fopen("data/user_data.json", "w+");
+
+    /* If file doesn't exist, there's an error */
     if (file == NULL)
     {
         printf("\nError while creating file\n");
         exit(1);
     }
+    fprintf(file, "{\n"); /* Start curly bracket */
+    fprintf(file, " \"days_simulated\": \"%d\",", days);
 
-    for (i = 0; i <= APPLIANCE_MAX; i++)
+    /* For dates */
+    for (i = 0; i < days /* DATES */; i++)
     {
-        fprintf(file, "Appliance: %d, Power Consumption: %f\n", user.plug[i].id, user.plug[i].power_consumption);
+        fprintf(file, "\n   \"%d\": {\n", calc_time(current_time, i, days)); /* Date */
+
+        /* For all appliances */
+        for (x = 0; x < APPLIANCE_MAX; x++)
+        {
+            fprintf(file, "     \"%s\": {\n         \"appliance_id\": %d,\n         \"power_consumption\": %f\n        }", appliances_string[x + 1], user.plug[x].id, user.plug[x].power_consumption);
+            /* If we are on the last element; don't end with comma as it is not the correct syntax in .json */
+            if (x != APPLIANCE_MAX - 1)
+                fprintf(file, ",");
+            fprintf(file, "\n");
+        }
+        fprintf(file, "    }");
+        if (i != days /* DATES */)
+            fprintf(file, ",");
     }
+    fprintf(file, "\n}"); /* Close curly bracket */
 
     fclose(file);
+}
+
+int calc_time(struct tm *current_time, int index, int days_simulated)
+{
+    current_time->tm_mday = current_time->tm_mday - (days_simulated - index);
+
+    time_t new_time = mktime(current_time);
+
+    return new_time;
 }
