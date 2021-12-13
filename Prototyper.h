e6f7244cdef9bb86e3f82cfe8908fa0a4c34a200
SadAndBroken <lasse.stocklund@hotmail.com>
2021-12-13 12:01:42 +0100

    makefiles my man

--- Prototyper.h
+++ Prototyper.h
@@ -2,4 +2,28 @@
 #define PROTOTYPE_H_
 #include "structs.h"
 
+double average_consumption(int, appliance[], user_profile);
+double total_consumption(int, appliance[]);
+void bar_chart(user_profile, double, int);
+void charts(user_profile, int, average_profile);
+double percent(double, double);
+double convert_power_to_cash(double);
+double convert_power_to_CO2(double);
+void print_break(void);
+void print_title(char[]);
+void print_section(char[]);
+json_t const *open_json_file_user(void);
+json_t const *open_json_file_average(void);
+int parse_json_days_simulated(json_t const *);
+user_profile parse_json_user_data(user_profile, user_profile *, json_t const *, int, int, int *);
+average_profile parse_json_average_data(average_profile, json_t const *);
+user_profile initialize_user_profile(user_profile, int *);
+user_profile add_plug(user_profile, int);
+void compare_plugs(user_profile, user_profile, average_profile, appliance *, appliance *, int, int *, int *, int);
+int place_in_correct_array(int, double, double, appliance *, appliance *, int **, int **);
+void print_percentage_of_average(int, int, double, double, int, int);
+void print_tips(appliance[], appliance[], int, int);
+void print_switch(appliance[], int);
+double calc_prev_avg(user_profile, user_profile, int, int *);
+
 #endif
--- structs.h
+++ structs.h
@@ -1,6 +1,47 @@
 #ifndef STRUCTS_H_
 #define STRUCTS_H_
 
+#define APPLIANCE_MAX 6                   /* Amount of appliances the system knows */
+#define PLUGS_MAX 10                      /* Max amount of plugs in the system */
+#define KWH_TO_DKK 2.3                    /* Price for 1 kWh in DKK */
+#define KWH_TO_CO2_G 122                  /* CO2 g emmission pr 1 kWh production */
+#define KWH_TO_CO2_KG KWH_TO_CO2_G / 1000 /* CO2 kg emmission pr 1 kWh production */
+#define PRINT_LINE_SIZE 96                /* Length of line to seperate text (must be even when divided by 2)*/
+
+
+enum appliances
+{
+    microwave = 1,
+    kettle,
+    oven,
+    refrigerator,
+    coffee
+};
+typedef enum appliances appliances;
+
+char *appliances_string[APPLIANCE_MAX] = {"empty", "Microwave", "Kettle", "Oven", "Refrigerator", "Coffee Machine"};
+char *appliances_string_lwr[APPLIANCE_MAX] = {"empty", "microwave", "kettle", "oven", "refrigerator", "coffee machine"};
+
+struct appliance
+{
+    appliances id;
+    double power_consumption;
+};
+typedef struct appliance appliance;
+
+struct user_profile
+{
+    int household_size;
+    appliance plug[PLUGS_MAX];
+};
+typedef struct user_profile user_profile;
+
+struct average_profile
+{
+    unsigned int household_size;
+    appliance appliances[APPLIANCE_MAX];
+};
+typedef struct average_profile average_profile;
 
 
 #endif
